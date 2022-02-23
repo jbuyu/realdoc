943f05e78866a992870506c61424b5f8736b0b00
jbuyu <jbuyu@ymail.com>
2022-02-23 15:08:44 +0300

    shifts wave to main page

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
+import Wave from "./Wave";
 class Layout extends Component {
   render() {
     const { children } = this.props;
@@ -10,6 +11,7 @@ class Layout extends Component {
         <Header />
         <NavBar />
         {children}
+        <Wave />
         <Footer />
       </div>
     );
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       {/* <Header /> */}
       <Body />
       <Choice />
-      <Wave />
+      {/* <Wave /> */}
       {/* <Footer /> */}
     </div>
   )
