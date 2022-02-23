cea95b4e36e6e343ea632982a507d20a4ec8e940
jbuyu <jbuyu@ymail.com>
2022-02-23 18:26:44 +0300

    adds team

--- pages/team.js
+++ pages/team.js
@@ -1,17 +1,16 @@
 import React from 'react'
 
 const Team = () => (
-  <div className="mx-auto flex max-w-7xl flex-col items-center justify-center bg-[url('/bgpattern2.svg')] bg-cover bg-no-repeat ">
-    <div className="mt-10 flex items-center justify-center text-gray-900">
-      <div>
+  <div className="max-w-8xl mx-auto flex flex-col  bg-[url('/bgpattern4.svg')] bg-cover bg-no-repeat ">
+    <div className="mt-10 flex flex-wrap justify-around px-20 text-gray-900">
+      <div className="pt-4">
         <div className="h-80 w-130">
           <img
-            src="/doc.jpeg"
+            src="/doc0.jpeg"
             alt=" random imgee"
             class="w-full rounded-lg object-cover object-center shadow-md"
           />
         </div>
-
         <div class="relative mt-36 px-10 ">
           <div class="rounded-lg bg-white p-6 shadow-lg">
             <h4 class="text-md mt-1 truncate font-semibold  leading-tight">
@@ -20,6 +19,143 @@ const Team = () => (
             <div class="flex items-baseline pt-2">
               <span class="inline-block rounded-full bg-teal-200 px-2 py-1 text-xs font-semibold  uppercase tracking-wide text-teal-800">
                 Physiotherapist
+              </span>
+            </div>
+            {/* <div class="mt-4">
+              <span class="text-md font-semibold text-teal-600">
+                4/5 ratings{' '}
+              </span>
+              <span class="text-sm text-gray-600">(based on 234 ratings)</span>
+            </div> */}
+          </div>
+        </div>
+      </div>
+      <div className="pt-4">
+        <div className="h-80 w-130">
+          <img
+            src="/doc1.jpeg"
+            alt=" random imgee"
+            class="w-full rounded-lg object-cover object-center shadow-md"
+          />
+        </div>
+        <div class="relative mt-36 px-10 ">
+          <div class="rounded-lg bg-white p-6 shadow-lg">
+            <h4 class="text-md mt-1 truncate font-semibold  leading-tight">
+              Dr. Stacy Alicia
+            </h4>
+            <div class="flex items-baseline pt-2">
+              <span class="inline-block rounded-full bg-teal-200 px-2 py-1 text-xs font-semibold  uppercase tracking-wide text-teal-800">
+                Cardiologist
+              </span>
+            </div>
+            {/* <div class="mt-4">
+              <span class="text-md font-semibold text-teal-600">
+                4/5 ratings{' '}
+              </span>
+              <span class="text-sm text-gray-600">(based on 234 ratings)</span>
+            </div> */}
+          </div>
+        </div>
+      </div>
+      <div className="pt-4">
+        <div className="h-80 w-130">
+          <img
+            src="/doc2.jpeg"
+            alt=" random imgee"
+            class="w-full rounded-lg object-cover object-center shadow-md"
+          />
+        </div>
+        <div class="relative mt-36 px-10 ">
+          <div class="rounded-lg bg-white p-6 shadow-lg">
+            <h4 class="text-md mt-1 truncate font-semibold  leading-tight">
+              Dr. Brian Molembe
+            </h4>
+            <div class="flex items-baseline pt-2">
+              <span class="inline-block rounded-full bg-teal-200 px-2 py-1 text-xs font-semibold  uppercase tracking-wide text-teal-800">
+                Dentist
+              </span>
+            </div>
+            {/* <div class="mt-4">
+              <span class="text-md font-semibold text-teal-600">
+                4/5 ratings{' '}
+              </span>
+              <span class="text-sm text-gray-600">(based on 234 ratings)</span>
+            </div> */}
+          </div>
+        </div>
+      </div>
+      <div className="pt-4">
+        <div className="h-80 w-130">
+          <img
+            src="/doc3.jpeg"
+            alt=" random imgee"
+            class="w-full rounded-lg object-cover object-center shadow-md"
+          />
+        </div>
+
+        <div class="relative mt-36 px-10 ">
+          <div class="rounded-lg bg-white p-6 shadow-lg">
+            <h4 class="text-md mt-1 truncate font-semibold  leading-tight">
+              Dr. Phillip Okinawa
+            </h4>
+            <div class="flex items-baseline pt-2">
+              <span class="inline-block rounded-full bg-teal-200 px-2 py-1 text-xs font-semibold  uppercase tracking-wide text-teal-800">
+                CT &amp; X-Ray
+              </span>
+            </div>
+            {/* <div class="mt-4">
+              <span class="text-md font-semibold text-teal-600">
+                4/5 ratings{' '}
+              </span>
+              <span class="text-sm text-gray-600">(based on 234 ratings)</span>
+            </div> */}
+          </div>
+        </div>
+      </div>
+      <div className="pt-4">
+        <div className="h-80 w-130">
+          <img
+            src="/doc4.jpeg"
+            alt=" random imgee"
+            class="w-full rounded-lg object-cover object-center shadow-md"
+          />
+        </div>
+
+        <div class="relative mt-36 px-10 ">
+          <div class="rounded-lg bg-white p-6 shadow-lg">
+            <h4 class="text-md mt-1 truncate font-semibold  leading-tight">
+              Dr. Julius Sumano
+            </h4>
+            <div class="flex items-baseline pt-2">
+              <span class="inline-block rounded-full bg-teal-200 px-2 py-1 text-xs font-semibold  uppercase tracking-wide text-teal-800">
+                Neurologist
+              </span>
+            </div>
+            {/* <div class="mt-4">
+              <span class="text-md font-semibold text-teal-600">
+                4/5 ratings{' '}
+              </span>
+              <span class="text-sm text-gray-600">(based on 234 ratings)</span>
+            </div> */}
+          </div>
+        </div>
+      </div>
+      <div className="pt-4">
+        <div className="h-80 w-130">
+          <img
+            src="/doc5.jpeg"
+            alt=" random imgee"
+            class="w-full rounded-lg object-cover object-center shadow-md"
+          />
+        </div>
+        <div class="relative mt-36 px-10 ">
+          <div class="rounded-lg bg-white p-6 shadow-lg">
+            <h4 class="text-md mt-1 truncate font-semibold  leading-tight">
+              Dr. Sandra Achieng'
+            </h4>
+            <div class="flex items-baseline pt-2">
+              <span class="inline-block rounded-full bg-teal-200 px-2 py-1 text-xs font-semibold  uppercase tracking-wide text-teal-800">
+                Nutritionist
               </span>
             </div>
             {/* <div class="mt-4">
