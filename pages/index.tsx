70b0a7b71b826d1b43b0e4c822b33dc7a6776476
jbuyu <jbuyu@ymail.com>
2022-02-24 11:00:47 +0300

    removes components from pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import Body from './components/Body'
-import Choice from './components/Choice'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import NavBar from './components/NavBar'
-import Wave from './components/Wave'
+import Body from '../components/Body'
+import Choice from '../components/Choice'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import NavBar from '../components/NavBar'
+import Wave from '../components/Wave'
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col justify-center font-mono text-slate-700 ">
