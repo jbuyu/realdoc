60ca485bd709aa3cd01697fa8fa0a38c2afd22fc
jbuyu <jbuyu@ymail.com>
2022-02-26 20:49:30 +0300

    styling on text

--- pages/About.js
+++ pages/About.js
@@ -15,10 +15,12 @@ const About = () => (
           className="rounded-4xl"
         />
       </div>
-      <div className="flex h-full w-1/2 flex-col justify-evenly">
-        <div>Preventative, Prescriptive &amp; Curative Medical Practices</div>
-        <div>
-          We take our work seriously, offering consultations, preventative care
+      <div className="item-center flex h-full w-1/2 flex-col justify-center font-mono">
+        <div className="text-2xl font-extrabold">
+          Preventative, Prescriptive &amp; Curative Medical Practices
+        </div>
+        <div className="py-8">
+          We take our work seriously; offering consultations, preventative care
           and therapeutic advice with utmost integrity. We see our work as a
           service to the community because a healthy society is our bet towards
           life well lived.
@@ -26,11 +28,11 @@ const About = () => (
       </div>
     </div>
     <div className="mt-12 flex h-96 items-center justify-around py-4">
-      <div className="flex h-full w-1/2 flex-col justify-evenly">
-        <div>
+      <div className="flex h-full w-1/2 flex-col items-center justify-center font-mono">
+        <div className="text-2xl font-extrabold">
           State of the art Equipment, Prompt results &amp; fast feedback
         </div>
-        <div>
+        <div className="py-8">
           We Believe in delivering services as quick and in the most perfect way
           we can. Alongside having state of the art equipment, our team is
           dedicated and at your service. Feel free to reach out
