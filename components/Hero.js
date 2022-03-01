a0a73ad43ef09c14aff1b32dff7c7d2c7265d8a4
jbuyu <jbuyu@ymail.com>
2022-03-01 08:49:40 +0300

    styles main page

--- components/Hero.js
+++ components/Hero.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 const Hero = () => (
   <div className="h-128">
-    <div className="mx-auto flex max-w-7xl justify-between">
+    <div className="mx-auto flex justify-between px-2 md:max-w-7xl md:px-0">
       <div className="flex h-128 flex-1 flex-col justify-center ">
         <div className="flex-start flex w-full space-x-2 border-l-4 border-solid border-indigo-700  ">
           <span></span>
@@ -18,7 +18,7 @@ const Hero = () => (
           </div>
         </div>
         <div className="pt-4">
-          <button className="inline-flex rounded bg-indigo-500 py-2 px-4 font-bold text-white transition ease-in-out hover:-translate-1 hover:scale-110 hover:bg-indigo-700">
+          <button className="hover:-translate-1 inline-flex rounded bg-indigo-500 py-2 px-2 font-bold text-white transition ease-in-out hover:scale-110 hover:bg-indigo-700 md:px-4">
             <Link href="/Form">
               <span>Book Appointment</span>
             </Link>
@@ -31,14 +31,14 @@ const Hero = () => (
           alt="dots_svg"
           className="absolute top-0 right-0 flex w-1/6"
         />
-        <div className=" absolute top-10 right-10">
+        <div className=" absolute top-20 md:top-10 right-0 md:right-10">
           <Image
             // loader={myLoader}
             src="/doc3.jpeg"
             alt="Picture of the author"
             width={432}
             height={624}
-            className=" rounded-2xl "
+            className=" rounded-2xl  "
           />
         </div>
       </div>
