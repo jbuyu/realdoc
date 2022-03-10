a487a2c6327a14deca62d696697830b8f1630423
jbuyu <jbuyu@ymail.com>
2022-03-10 09:55:56 +0300

    adds get request

--- pages/api/consultations/index.js
+++ pages/api/consultations/index.js
@@ -3,10 +3,22 @@ import dbConnect from '../../../util/mongo'
 import Consultation from '../../../models/Consultation'
 
 export default async function handler(req, res) {
-  const { method } = req;
-  dbConnect();
+  const { method } = req
+  dbConnect()
 
   if (method === 'GET') {
+    try {
+      const consultations = await Consultation.find({})
+      if (consultations) {
+        res.json(consultations)
+      } else {
+        res.status(500).json({
+          message: 'Consultations not found',
+        })
+      }
+    } catch (error) {
+      res.status(500).json(error)
+    }
   }
   if (method === 'POST') {
     try {
