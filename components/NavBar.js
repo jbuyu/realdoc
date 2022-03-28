efe43eeffe1d04b030bc5d74f89464286fe48558
jbuyu <jbuyu@ymail.com>
2022-03-28 11:49:30 +0300

    adds logos

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,20 +3,20 @@ import Link from 'next/link'
 import { MenuIcon } from '@heroicons/react/solid'
 
 const NavBar = () => {
-  const [isopen, setIsOpen ] = useState(false)
+  const [isopen, setIsOpen] = useState(false)
   const handleHamburger = () => {
     setIsOpen(!isopen)
   }
   return (
-    <nav className="font-semibold ">
+    <nav className="font-semibold">
       <div className="mx-auto max-w-7xl py-2 md:py-6">
         <div className="flex items-center justify-between">
           <Link href="/">
             <div className="flex w-2/5 cursor-pointer items-center space-x-4">
-              <img src="/adonis.svg" alt="Vercel Logo" className="ml-2 h-12 " />
-              <div className=" hidden text-[#224E73] md:flex">
+              <img src="/logo3.jpeg" alt="Vercel Logo" className="ml-2 h-32 " />
+              {/* <div className=" hidden text-[#224E73] md:flex">
                 Star Homecare Services
-              </div>
+              </div> */}
             </div>
           </Link>
           <div className="hidden w-full justify-around text-[#224E73] md:flex md:w-2/5">
@@ -43,7 +43,7 @@ const NavBar = () => {
               <MenuIcon width="40" className="font-bold" />
             </button>
           </div>
-          <div className="hidden w-1/5 md:flex">
+          <div className="hidden  md:flex">
             <button className="rounded-full bg-indigo-600 py-2 px-6 font-bold text-white hover:bg-indigo-700">
               <Link className="cursor-pointer" href="/services">
                 Contact
@@ -51,7 +51,13 @@ const NavBar = () => {
             </button>
           </div>
         </div>
-        <div className={isopen ? "mobile-menu block space-y-3 p-4 md:hidden": "mobile-menu hidden space-y-3 p-4 md:hidden"}>
+        <div
+          className={
+            isopen
+              ? 'mobile-menu block space-y-3 p-4 md:hidden'
+              : 'mobile-menu hidden space-y-3 p-4 md:hidden'
+          }
+        >
           {/* <Link className="cursor-pointer" href="/Team">
             <div className="cursor-pointer hover:text-indigo-700 hover:underline">
               Team
