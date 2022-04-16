114c4b06a4103457f39b5d5aafde6ab76a9faa4a
jbuyu <jbuyu@ymail.com>
2022-04-16 17:16:42 +0300

    changes about width

--- pages/About.js
+++ pages/About.js
@@ -5,7 +5,7 @@ import SEO from '@bradgarropy/next-seo'
 const About = () => (
   <>
     <SEO title="About" description="About Star Homecare Services" />
-    <div className="mx-auto flex max-w-9xl flex-col items-center font-mono px-8">
+    <div className="mx-auto flex max-w-7xl flex-col items-center font-mono">
       <div className="mt-40 mb-10 md:mb-0 flex h-96 flex-col items-center justify-around py-4 px-4 md:mt-12 md:flex-row">
         <div className="flex h-full items-center md:w-3/5">
           <Image
