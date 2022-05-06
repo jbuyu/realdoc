0b345652faa1a2c9c20c74c9f16533caa3efcc23
jbuyu <jbuyu@ymail.com>
2022-05-06 14:10:54 +0300

    edits org url

--- components/Header.js
+++ components/Header.js
@@ -15,7 +15,7 @@ const Header = () => (
       content="kenyan Doctor online, best kenya doctor help, telemedicine in kenya, best consult doctor Kenya, best online medical consultation kenya, best online consultation kenya, best home based nursing Care in kenya, best homedoctors Services in kenya, best nutritionist in kenya online, best physiotherapy in kenya, best home based antenatal care in kenya"
     />
     <link rel="icon" href="/favicon.ico" />
-    <meta property="og:url" content="https://www.starhomecareservices.com/" />
+    <meta property="og:url" content="https://www.starhomecareservices.com" />
     <meta property="og:type" content="website" />
     <meta
       property="og:title"
@@ -30,7 +30,7 @@ const Header = () => (
       content="https://www.starhomecareservices.com/favicon.jpg"
     />
     <meta name="twitter:card" content="summary" />
-    <meta name="twitter:site" content="https://www.starhomecareservices.com/" />
+    <meta name="twitter:site" content="https://www.starhomecareservices.com" />
     <meta
       name="twitter:title"
       content="Best Online Doctor Consultation in Kenya - Star Homecare Services"
